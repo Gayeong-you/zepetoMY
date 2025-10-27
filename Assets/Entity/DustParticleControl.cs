5c9a33952085e220b98ddbb6a73238f54ef3ccff
Gayeong-you <yko66901@gmail.com>
2025-10-27 22:18:47 +0900

    Feat: 파티클 작업중 (수정필요)

---
+++ Assets/Entity/DustParticleControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entity
+{
+    public class DustParticleControl : MonoBehaviour
+    {
+        [SerializeField] private bool createDustOnWalk = true;
+        [SerializeField] private ParticleSystem dustParticleSystem;
+
+        public void CreateDustParticles()
+        {
+            if (createDustOnWalk)
+            {
+                dustParticleSystem.Stop();
+                dustParticleSystem.Play();
+            }
+        }
+    }
+
+}
